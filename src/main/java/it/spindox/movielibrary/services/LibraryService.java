84ae34f4369c71ecbb99128e0e1f931bae2d136f
antoliu <a.liuzzi93@gmail.com>
2021-11-18 13:18:53 +0100

    Modifiche JavaDoc

--- src/main/java/it/spindox/movielibrary/services/LibraryService.java
+++ src/main/java/it/spindox/movielibrary/services/LibraryService.java
@@ -28,7 +28,7 @@ public class LibraryService {
      * If the movie list in repository is not empty, the HttpStatus is <code>200</code> and the response body contains the movie list.
      * </p>
      * @return A Response Entity with <strong>movie list</strong> or, if list is empty, an <strong>error message</strong>.
-     * @throws RepositoryException
+     * @throws RepositoryException if the repository is empty
      */
     public ResponseEntity<Collection<Movie>> getAllMovies() throws RepositoryException {
         Collection<Movie> movieList = repository.getAllMovies();
@@ -42,14 +42,12 @@ public class LibraryService {
      * </p>
      * @param movie     The movie to be insert
      * @return A Response Entity with <strong>confirmation message</strong>
-     * @throws RepositoryException
-     * @throws FieldsException
+     * @throws FieldsException if the title or the director fields are null or empty or blank.
      */
     public ResponseEntity<String> addMovie(Movie movie) throws FieldsException {
         repository.checkFields(movie);
         repository.addMovie(movie);
-        ResponseEntity<String> response = new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
     }
 
     /**
@@ -59,7 +57,7 @@ public class LibraryService {
      * </p>
      * @param movies    The list of movies to be insert
      * @return A Response Entity with <strong>confirmation message</strong> or, if list is empty, an <strong>error message</strong>.
-     * @throws FieldsException
+     * @throws FieldsException if the body of request is not valid. If title or director of a movie are null, blank or empty.
      */
     public ResponseEntity<String> addMovies(Collection<Movie> movies) throws FieldsException {
         if(movies.isEmpty()){
@@ -69,8 +67,7 @@ public class LibraryService {
             repository.checkFields(movie);
             repository.addMovie(movie);
         }
-        ResponseEntity<String> response = new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
     }
 
     /**
@@ -80,39 +77,36 @@ public class LibraryService {
      * </p>
      * @param id    The ID whose associated movie is to be returned
      * @return Response Entity with the<strong>movie</strong> related to the specified ID or, if Id is not present, an <strong>error message</strong>.
-     * @throws MovieException
-     * @throws RepositoryException
+     * @throws MovieException if there is not even a movie with the entered ID
+     * @throws RepositoryException if the repository is empty
      */
     public ResponseEntity<Movie> getMovie(int id) throws MovieException, RepositoryException {
         Movie movie = repository.getMovie(id);
-        ResponseEntity<Movie> response = new ResponseEntity<>(movie, HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>(movie, HttpStatus.OK);
     }
 
     /**
      * Returns a <code>Response Entity</code> with an <code>HttpStatus</code> and a <code>Collection</code> of movies which matches the searched title.
      * @param title     The title to be searched
      * @return Returns a <code>Response Entity</code> with an <code>HttpStatus</code> and a <code>Collection</code> of movies
-     * @throws MovieException
-     * @throws RepositoryException
+     * @throws MovieException if there is not even a movie with the entered title
+     * @throws RepositoryException if the repository is empty
      */
     public ResponseEntity<Collection<Movie>> getMovieByTitle(String title) throws MovieException, RepositoryException {
         List<Movie> result = repository.getMovieByTitle(title);
-        ResponseEntity<Collection<Movie>> response = new ResponseEntity<>(result, HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     /**
      * Returns a <code>Response Entity</code> with an <code>HttpStatus</code> and a <code>Collection</code> of movies which matches the searched director.
      * @param director     The director to be searched
      * @return Returns a <code>Response Entity</code> with an <code>HttpStatus</code> and a <code>Collection</code> of movies
-     * @throws MovieException
-     * @throws RepositoryException
+     * @throws MovieException if there is not even a movie with the entered director
+     * @throws RepositoryException if the repository is empty
      */
     public ResponseEntity<Collection<Movie>> getMovieByDirector(String director) throws MovieException, RepositoryException {
         List<Movie> result = repository.getMovieByDirector(director);
-        ResponseEntity<Collection<Movie>> response = new ResponseEntity<>(result, HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     /**
@@ -121,14 +115,13 @@ public class LibraryService {
      * If the Id of the movie to be deleted is present, the HttpStatus is <code>200</code> and the response body contains a confirmation message.
      * </p>
      * @param id        The ID whose associated movie is to be removed
-     * @returnA Response Entity with <strong>confirmation message</strong> or, if list is empty, an <strong>error message</strong>.
-     * @throws MovieException
-     * @throws RepositoryException
+     * @return Response Entity with <strong>confirmation message</strong> or, if list is empty, an <strong>error message</strong>.
+     * @throws MovieException if there is not even a movie with the entered ID
+     * @throws RepositoryException if the repository is empty
      */
     public ResponseEntity<String> deleteMovie(int id) throws MovieException, RepositoryException {
         repository.removeMovie(id);
-        ResponseEntity<String> response = new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
     }
 
     /**
@@ -140,17 +133,16 @@ public class LibraryService {
      * @param movie     The Movie with the fields to be update
      * @param patch     A flag to check if is a partial, <code>(patch=true)</code>, or total, <code>(patch=false)</code>, update
      * @return A Response Entity with <strong>confirmation message</strong> or, if list is empty, an <strong>error message</strong>.
-     * @throws MovieException
-     * @throws FieldsException
-     * @throws RepositoryException
+     * @throws MovieException if there is not even a movie with the entered ID
+     * @throws RepositoryException if the repository is empty
+     * @throws FieldsException if the title or director fields are null or empty or blank
      */
     public ResponseEntity<String> updateMovie(int id, Movie movie, boolean patch) throws MovieException, FieldsException, RepositoryException {
         if(!patch){
             repository.checkFields(movie);
         }
         repository.updateMovie(id, movie);
-        ResponseEntity<String> response = new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
-        return response;
+        return new ResponseEntity<>("CONFIRM: Operation Done", HttpStatus.OK);
     }
 
 }
